01fe362eb1c633f7259aad15a3ae0a212e6a367d
asadbek-ip <dasturchi6678@gmail.com>
2021-11-08 10:50:38 +0500

    Bu 4 chi oyna

---
+++ lib/Pages/fourPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class FourPage extends StatefulWidget {
+  const FourPage({Key? key}) : super(key: key);
+
+  @override
+  _FourPageState createState() => _FourPageState();
+}
+
+class _FourPageState extends State<FourPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Text('Bu 4 chi oyna'),
+  }
+}
